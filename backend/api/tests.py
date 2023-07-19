9715b063343dbacc575b3c3ae6cad9bb35ba4e60
Fomichev Vadim <fomichev1499@yandex.ru>
2023-07-19 16:22:32 +0300

    add tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
-from django.test import Client, TestCase
+
 from api import models
+from django.test import Client
+from django.test import TestCase
 
 
 class TaskiAPITestCase(TestCase):
